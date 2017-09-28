e78f219388a1d1dcdf588ed15180dc7eab07f000
fanny <fc2@cin.ufpe.br>
2017-09-28 10:19:58 -0300

    Retrieving the url in the closure

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/domain/XmlFeedParser.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/domain/XmlFeedParser.java
@@ -1,5 +1,7 @@
 package br.ufpe.cin.if710.podcast.domain;
 
+import android.util.Log;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -80,7 +82,7 @@ public class XmlFeedParser {
                 description = readData(parser, "description");
             }
             else if (name.equals("enclosure")) {
-                //TODO implementar resto do metodo para pegar atributo url da tag enclosure
+                //TODO 1 implementar resto do metodo para pegar atributo url da tag enclosure
                 downloadLink = readEnclosure(parser);
                 skip(parser);
             }
@@ -114,8 +116,10 @@ public class XmlFeedParser {
     // Processa tags do tipo <enclosure> para obter dados do episodio
     public static String readEnclosure(XmlPullParser parser)
             throws IOException, XmlPullParserException {
-        //parser.require(XmlPullParser.START_TAG, null, "enclosure");
-        String data = "implementar...";
+
+        //obtendo a url que esta no enclosure
+        parser.require(XmlPullParser.START_TAG, null, "enclosure");
+        String data = parser.getAttributeValue(null, "url");
         //parser.require(XmlPullParser.END_TAG, null, "enclosure");
         return data;
     }
