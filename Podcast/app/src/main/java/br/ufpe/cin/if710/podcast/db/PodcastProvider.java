b69b2c08cc4cac9984d99c57fc324290db430357
fanny <fc2@cin.ufpe.br>
2017-10-02 00:43:26 -0300

    Implementing the insert functionality

--- Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/db/PodcastProvider.java
+++ Podcast/app/src/main/java/br/ufpe/cin/if710/podcast/db/PodcastProvider.java
@@ -1,13 +1,61 @@
 package br.ufpe.cin.if710.podcast.db;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.util.Log;
+
 
 public class PodcastProvider extends ContentProvider {
     public PodcastProvider() {
     }
+
+    private PodcastDBHelper podcastDBHelper;
+
+
+    @Override
+    public boolean onCreate() {
+        // Implement this to initialize your content provider on startup.
+        Context context;
+        Context context = this.getContext();
+        podcastDBHelper = PodcastDBHelper.getInstance(context);
+        return true;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        // Implement this to handle requests to insert a new row.
+        final SQLiteDatabase database = podcastDBHelper.getWritableDatabase();
+
+        Uri returnUri;
+        long id = database.insert(PodcastProviderContract.EPISODE_TABLE, null, values);
+        //verificar se conseguimos adicionar
+        if (id > 0){
+            //contruindo a nova uri
+            returnUri = ContentUris.withAppendedId(PodcastProviderContract.EPISODE_LIST_URI, id);
+            Log.d("PodcastProvider", "inserting" + returnUri.toString());
+
+        } else {
+            throw new android.database.SQLException("Failed to insert row to" + uri);
+        }
+
+        //notificar ao resolver que aconteceu mudancas no uri
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        return returnUri;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        // TODO: Implement this to handle query requests from clients.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
@@ -22,24 +70,6 @@ public class PodcastProvider extends ContentProvider {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    @Override
-    public boolean onCreate() {
-        // TODO: Implement this to initialize your content provider on startup.
-        return false;
-    }
-
-    @Override
-    public Cursor query(Uri uri, String[] projection, String selection,
-                        String[] selectionArgs, String sortOrder) {
-        // TODO: Implement this to handle query requests from clients.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection,
